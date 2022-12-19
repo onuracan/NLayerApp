e001caed65758cbd59a03654ef18d9e64e0043b8
Onur Açan <43632882+onuracan@users.noreply.github.com>
2022-12-19 17:40:21 +0300

    .

---
+++ NLayer.Service/Validation/ProductDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs;
+
+
+namespace NLayer.Service.Validation;
+
+public class ProductDtoValidator : AbstractValidator<ProductDto>
+{
+	public ProductDtoValidator()
+	{
+		RuleFor(x => x.Name).NotNull().WithName("Ürün Adı").WithMessage("{PropertyName} Boş olmamalıdır.").NotEmpty().WithMessage("{PropertyName}");
+		RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithName("Fiyat").WithMessage("Belirlenmiş olmalıdır.");
+		RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithName("Stok").WithMessage("Belirlenmiş olmalıdır.");
+		RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithName("Kategori").WithMessage("Belirlenmiş olmalıdır.");
+	}
+}
